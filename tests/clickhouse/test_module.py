9321083dbd39ce4ec6fd277f0424752ec48d0ce5
k0rsakov <korsakov.iyu@gmail.com>
2023-02-18 14:49:48 +0800

    Changed project connectors_to_databases: - edit tests - add docstring for tests

--- tests/clickhouse/test_module.py
+++ tests/clickhouse/test_module.py
@@ -4,7 +4,11 @@ from connectors_to_databases.ClickHouse import ClickHouse
 
 
 def test_execute_script():
-    """"""
+    """
+    Checking the script for execution.
+
+    The test creates a table and checks if it has been created.
+    """
     ch = ClickHouse(
         login='click',
         password='click',
@@ -35,14 +39,16 @@ def test_execute_script():
 
     assert len(df) == 1
 
-def test_insert_pg_table():
-    """"""
+def test_insert_ch_table():
+    """
+    Checking the method for inserting data.
+    """
     ch = ClickHouse(
         login='click',
         password='click',
     )
 
-    d = {'value': [i for i in range(10000)]}
+    d = {'value': [i for i in range(10000000)]}
     df = pd.DataFrame(d)
     ch.insert_df(
         df=df,
@@ -52,11 +58,13 @@ def test_insert_pg_table():
 
     df = ch.execute_to_df('SELECT * FROM test')
 
-    assert len(df) == 10000
+    assert len(df) == 10000000
 
 
 def test_execute_df():
-    """"""
+    """
+    Checking the method for extracting data into a dataframe.
+    """
 
     ch = ClickHouse(
         login='click',
@@ -69,7 +77,9 @@ def test_execute_df():
     assert isinstance(df, pd.DataFrame)
 
 def test_get_uri():
-    """"""
+    """
+    Checking for getting a uri for use outside of class methods.
+    """
 
     ch = ClickHouse(
         login='click',
--- tests/postgresql/test_module.py
+++ tests/postgresql/test_module.py
@@ -1,16 +1,16 @@
-"""
-Модульные тесты проверяют функциональность отдельного модуля кода,
-изолированного от его зависимостей
-"""
 import pandas as pd
 
-from connectors_to_databases.postgre_base import PostgreSQL
+from connectors_to_databases.PostgreSQL import PostgreSQL
 
 
 def test_execute_script():
-    """"""
+    """
+    Checking the script for execution.
+
+    The test creates a table and checks if it has been created.
+    """
     pg = PostgreSQL(
-        port=1
+        port=1,
     )
 
     pg.execute_script('CREATE TABLE test(id bigserial PRIMARY KEY, value int8)')
@@ -30,14 +30,16 @@ def test_execute_script():
     assert len(df) == 1
 
 def test_insert_pg_table():
-    """"""
+    """
+    Checking the method for inserting data.
+    """
     pg = PostgreSQL(
         port=1
     )
 
     d = {'value': [i for i in range(10000)]}
     df = pd.DataFrame(d)
-    pg.into_pg_table(
+    pg.insert_df(
         df=df,
         chunksize=None,
         pg_table_name='test'
@@ -49,7 +51,9 @@ def test_insert_pg_table():
 
 
 def test_execute_df():
-    """"""
+    """
+    Checking the method for extracting data into a dataframe.
+    """
 
     pg = PostgreSQL(
         port=1
@@ -61,7 +65,9 @@ def test_execute_df():
     assert isinstance(df, pd.DataFrame)
 
 def test_get_uri():
-    """"""
+    """
+    Checking for getting a uri for use outside of class methods.
+    """
 
     pg = PostgreSQL(
         port=1
