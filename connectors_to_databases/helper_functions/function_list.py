88b3852ce4e9388d919944733cec55a730449965
k0rsakov <korsakov.iyu@gmail.com>
2023-08-25 04:54:12 +0300

    Changed project connectors_to_databases: - change description by list_values_in_str_with_double_quotes

--- connectors_to_databases/helper_functions/function_list.py
+++ connectors_to_databases/helper_functions/function_list.py
@@ -2,21 +2,21 @@ def list_values_in_str_with_double_quotes(list_columns: list = None) -> str:  # 
     """
     **Function: list_values_in_str_with_double_quotes**
 
-    This static function takes a list of columns as the `list_columns` parameter and returns a string where each
-    column value is enclosed in double quotes.
+    This function takes a list of values as the `list_columns` parameter and returns a string where each
+    value is enclosed in double quotes.
 
     **Parameters:**
-    - `list_columns` (list, optional): The list of columns to be enclosed in double quotes.
+    - `list_columns` (list): The list of values to be enclosed in double quotes.
     If not specified, defaults to `None`.
 
     **Return:**
-    - `str`: A string containing column values enclosed in double quotes and separated by commas.
+    - `str`: A string containing values enclosed in double quotes and separated by commas.
 
     **Example Usage:**
 
     ```python
     columns = ['column1', 'column2', 'column3']
-    result = MyClass.list_columns_in_str_with_double_quotes(columns)
+    result = list_values_in_str_with_double_quotes(columns)
     print(result)
     ```
 
@@ -26,14 +26,14 @@ def list_values_in_str_with_double_quotes(list_columns: list = None) -> str:  # 
     "column1", "column2", "column3"
     ```
 
-    In this example, we pass the `columns` list of columns to the `list_columns_in_str_with_double_quotes`
+    In this example, we pass the `columns` list of columns to the `list_values_in_str_with_double_quotes`
     function and store the result in the `result` variable. Then, we print the value of `result`, which will
     contain the strings from the `columns` list enclosed in double quotes and separated by commas.
 
 
     @param list_columns: The list of columns to be enclosed in double quotes. If not specified, defaults
         to `None`.; default 'None'
-    @return: A string containing column values enclosed in double quotes and separated by commas.
+    @return: A string containing values enclosed in double quotes and separated by commas.
     """
 
     return ', '.join([f"\"{value}\"" for value in list_columns])
