5efc4b135f661fc82bb5926c198f2e3fc35eb2ec
k0rsakov <korsakov.iyu@gmail.com>
2023-09-06 05:43:00 +0300

    Changed project connectors_to_databases: - Add test_execute_script for MariaDB

--- tests/mariadb/test_module_mariadb.py
+++ tests/mariadb/test_module_mariadb.py
@@ -1,4 +1,3 @@
-import pandas as pd
 
 from connectors_to_databases.MariaDB import MariaDB
 
@@ -14,66 +13,61 @@ def test_execute_script():
         port=2,
     )
 
-    m.execute_script('CREATE TABLE test(id bigserial PRIMARY KEY, value int8)')
+    m.execute_script('CREATE TABLE IF NOT EXISTS  test(id int8 PRIMARY KEY, value int8)')
 
     df = m.execute_to_df(  # noqa: PD901
         '''
-        SELECT
-            *
-        FROM
-            information_schema."tables" AS t
-        WHERE
-            table_schema = 'public'
-            AND table_name = 'test'
+        SHOW TABLES
+        WHERE tables_in_sys = 'test'
         '''
     )
 
     assert len(df) == 1
 
-def test_insert_m_table():
-    """
-    Checking the method for inserting data.
-    """
-    m = MariaDB(
-        host='127.0.0.1',
-        port=2,
-    )
-
-    d = {'value': list(range(10000))}
-    df = pd.DataFrame(d) # noqa: PD901
-    m.insert_df(
-        df=df,
-        chunksize=None,
-        table_name='test'
-    )
-
-    df = m.execute_to_df('SELECT * FROM test') # noqa: PD901
-
-    assert len(df) == 10000
-
-
-def test_execute_df():
-    """
-    Checking the method for extracting data into a dataframe.
-    """
-
-    m = MariaDB(
-        host='127.0.0.1',
-        port=2,
-    )
-
-    df = m.execute_to_df('SELECT count(value) FROM test') # noqa: PD901
-
-    assert len(df) == 1
-    assert isinstance(df, pd.DataFrame)
-
-def test_get_uri():
-    """
-    Checking for getting a uri for use outside class methods.
-    """
-
-    m = MariaDB(
-        port=1
-    )
-
-    m.get_uri().execute('DROP TABLE IF EXISTS test')
+# def test_insert_m_table():
+#     """
+#     Checking the method for inserting data.
+#     """
+#     m = MariaDB(
+#         host='127.0.0.1',
+#         port=2,
+#     )
+# 
+#     d = {'value': list(range(10000))}
+#     df = pd.DataFrame(d) # noqa: PD901
+#     m.insert_df(
+#         df=df,
+#         chunksize=None,
+#         table_name='test'
+#     )
+# 
+#     df = m.execute_to_df('SELECT * FROM test') # noqa: PD901
+# 
+#     assert len(df) == 10000
+# 
+# 
+# def test_execute_df():
+#     """
+#     Checking the method for extracting data into a dataframe.
+#     """
+# 
+#     m = MariaDB(
+#         host='127.0.0.1',
+#         port=2,
+#     )
+# 
+#     df = m.execute_to_df('SELECT count(value) FROM test') # noqa: PD901
+# 
+#     assert len(df) == 1
+#     assert isinstance(df, pd.DataFrame)
+# 
+# def test_get_uri():
+#     """
+#     Checking for getting a uri for use outside class methods.
+#     """
+# 
+#     m = MariaDB(
+#         port=1
+#     )
+# 
+#     m.get_uri().execute('DROP TABLE IF EXISTS test')
